e0a5813875106e8d6b7dae9138f1e4500a10b8dd
ArpKevin <arpaskevin11@gmail.com>
2024-11-08 09:32:00 +0100

    Add project files.

---
+++ bevolkerung/Bevolkerung.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bevolkerung
+{
+    internal class Bevolkerung
+    {
+        public Bevolkerung(string sor)
+        {
+            var x = sor.Split(";");
+            Id = int.Parse(x[0]);
+            Nem = x[1];
+            SzuletesiEv = int.Parse(x[2]);
+            Suly = int.Parse(x[3]);
+            Magassag = int.Parse(x[4]);
+            Dohanyzik = x[5] == "igen";
+            Nemzetiseg = x[6];
+            Nepcsoport = x[7];
+            Tartomany = x[8];
+            NettoJovedelem = int.Parse(x[9]);
+            IskolaiVegzettseg = x[10];
+            PolitikaiNezet = x[11];
+            AktivSzavazo = x[12] == "igen";
+            SorFogyasztasEvente = int.Parse(x[13]);
+            KrumpliFogyasztasEvente = int.Parse(x[14]);
+        }
+
+        public int Id { get; set; }
+        public string Nem { get; set; }
+        public int SzuletesiEv { get; set; }
+        public int Suly { get; set; }
+        public int Magassag { get; set; }
+        public bool Dohanyzik { get; set; }
+        public string Nemzetiseg { get; set; }
+        public string? Nepcsoport { get; set; }
+        public string Tartomany { get; set; }
+        public int NettoJovedelem { get; set; }
+        public string? IskolaiVegzettseg { get; set; }
+        public string PolitikaiNezet { get; set; }
+        public bool AktivSzavazo { get; set; }
+        public int? SorFogyasztasEvente { get; set; }
+        public int? KrumpliFogyasztasEvente { get; set; }
+    }
+}
+
---
+++ bevolkerung/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bevolkerung
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Bevolkerung> citizens = new();
+
+            using StreamReader sr = new(@"..\..\..\src\bevölkerung.txt");
+
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                citizens.Add(new(sr.ReadLine()));
+            }
+
+            dataContainer.ItemsSource = citizens.Select(x => x.Id);
+            hanySor.Content = $"{citizens.Count()} sora van a listának.";
+            var elsoSor = citizens.First(); 
+            elsoSorAdatai.Content = $"{elsoSor.Id} {elsoSor.Nem} {elsoSor.SzuletesiEv} {elsoSor.Suly} {elsoSor.Magassag} {elsoSor.Dohanyzik} {elsoSor.Nemzetiseg} {elsoSor.Nepcsoport} {elsoSor.Tartomany} {elsoSor.NettoJovedelem} {elsoSor.IskolaiVegzettseg} {elsoSor.PolitikaiNezet} {elsoSor.AktivSzavazo} {elsoSor.SorFogyasztasEvente} {elsoSor.KrumpliFogyasztasEvente}";
+        }
+    }
+}
